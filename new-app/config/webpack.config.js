d39f52ead4730649f57f3c8d55b86effdd3a806f
Sonia Lukaszewicz <slukaszewicz@microstrategy.com>
2020-02-28 19:49:31 +0100

    changes

--- new-app/config/webpack.config.js
+++ new-app/config/webpack.config.js
@@ -80,6 +80,8 @@ module.exports = function(webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
+        modules: true,
+        localIdentName: '[name]__[local]__[hash:base64:5]'
       },
       {
         // Options for PostCSS as we reference these options twice
